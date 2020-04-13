aa58412a9ae3e8601b977e22da27a868833f85e7
Savuth-S <SavuthS4ns@protonmail.com>
2020-04-12 23:46:47 -0500

    Desarrollo de algoritmo con función

--- laboratorio10.py
+++ laboratorio10.py
@@ -10,3 +10,8 @@ def a_power_b(n1,n2):
     for i in range(0,n2,1):
         n1 *= n1
     return n1
+    
+###Programa Principal
+#Declaración de variables
+pot = int(input("Ingrese el numero que desea elevar: \n\n"))
+elv = int(input("\n\nIngrese la potencia a la que desea elevar el numero: \n\n"))
