f7a56431315af374ec801b7283fc33dcaabb5dd6
Savuth-S <SavuthS4ns@protonmail.com>
2020-04-12 23:45:53 -0500

    P-7

---
+++ laboratorio10.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 8 11:49:21 2020
+
+@author: Savuth S.
+"""
+
+###Funciones
+def a_power_b(n1,n2):
+    for i in range(0,n2,1):
+        n1 *= n1
+    return n1
